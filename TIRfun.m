eefdba473ecd56a75406d662cbed7690538fae26
Mike Bramble <48955908+mikebramble@users.noreply.github.com>
2023-03-18 09:46:15 -0700

    Delete TIRfun.m

--- TIRfun.m
+++
@@ -1,6 +0,0 @@
-function err = TIRfun(x,wavenumber,data,e_REFL)
-kB = 1.38064852E-23; % Boltzmann constant (J/K)
-h = 6.626070040E-34; % Planck constant (Js)
-c =  299792458; % Speed of light in vacuum (m/s)
-err = immse(((e_REFL*x(1)) .* ((2.e+8*h*c*c*wavenumber.^3) ./ (exp(100*h*wavenumber*c./(kB*x(2)))-1))),data);
-end
